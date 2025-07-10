f169c65c5921ecd05a34deff29e9d7f63e7c9bbd
swkruger <kruger.sw@gmail.com>
2025-07-10 18:42:54 -0500

    Ready for deployment v0.0.1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
+  // Exclude test directories from build
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack: (config, { isServer }) => {
+    // Exclude test directories from build
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      exclude: [
+        /node_modules/,
+        /test/,
+        /\.test\./,
+        /\.spec\./
+      ],
+    });
+    
+    return config;
+  },
+  // Production optimizations
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  // Environment variables validation
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 }
 
 module.exports = nextConfig 
